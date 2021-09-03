18c092f2ffde6a8ed2cad7acaeedd9bf7d74e2dd
Vadim <vad3724@gmail.com>
2021-09-03 12:18:31 +0300

    test size mirror

--- SharedPaintBoard.js
+++ SharedPaintBoard.js
@@ -15,7 +15,7 @@ class SharedPaintBoard {
             +"</div>");
         $('.'+data.block_paint).append("<canvas id='room_videochat_canvas1' style='border:1px solid #000000; display: inline-block;float:left;'></canvas>");
 
-        $('.'+data.block_mirror).append("<canvas id='room_videochat_canvas2' style='border:1px solid #000000; display: inline-block;float:left;width:150px;height:150px'></canvas>");
+        $('.'+data.block_mirror).append("<canvas id='room_videochat_canvas2' style='border:1px solid #000000; display: inline-block;float:left;width:300px;height:300px'></canvas>");
 
         $('.'+data.block_mirror+' canvas').css("pointer-events", "none");
 
@@ -23,9 +23,10 @@ class SharedPaintBoard {
         var canvas_left = new fabric.Canvas('room_videochat_canvas1');
         var canvas_right = new fabric.Canvas('room_videochat_canvas2');
 
-        $('.canvas-container').css("width", "150px");
-        $('.canvas-container #room_videochat_canvas2').css("width", "150px");
-        $('.canvas-container .upper-canvas').css("width", "150px");
+        $('.canvas-container').css("width", "300px");
+        $('.canvas-container #room_videochat_canvas2').css("width", "300px");
+        $('.canvas-container .upper-canvas').css("width", "300px");
+
 
 
 // is_down := keeps track if the mouse is down
