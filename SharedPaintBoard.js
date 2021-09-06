41371687442bc491bfa62d56da310cb201e68163
Vadim <vad3724@gmail.com>
2021-09-06 14:00:51 +0300

    test variable

--- SharedPaintBoard.js
+++ SharedPaintBoard.js
@@ -13,14 +13,12 @@ class SharedPaintBoard {
                     +"<div style='float:left;font-weight:bold'>Width</div> <div class='shared-paint-setting-color-check' style='width:15px;height:16px;background-color:#000;float:right;color:#fff;text-align:center;'>2</div>"
                 +"</div>"
             +"</div>");
-        $('.'+data.block_paint).append("<canvas id='room_videochat_canvas1' style='border:1px solid #000000; display: inline-block;float:left;'></canvas>");
-
         $('.'+data.block_mirror).append("<canvas id='room_videochat_canvas2' style='border:1px solid #000000; display: inline-block;float:left;width:150px;height:150px'></canvas>");
+
+        $('.'+data.block_paint).append("<canvas id='room_videochat_canvas1' style='border:1px solid #000000; display: inline-block;float:left;'></canvas>");
 
         $('.'+data.block_mirror+' canvas').css("pointer-events", "none");
 
-
-        alert('.'+data.block_mirror);
 
         var canvas_left = new fabric.Canvas('room_videochat_canvas1');
         var canvas_right = new fabric.Canvas('room_videochat_canvas2');
