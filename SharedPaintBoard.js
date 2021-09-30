b172c6ed2c2a069cf26b1c463742737d21149710
Vadim <vad3724@gmail.com>
2021-09-30 15:35:52 +0300

    test get array

--- SharedPaintBoard.js
+++ SharedPaintBoard.js
@@ -188,7 +188,7 @@ class SharedPaintBoard {
 
     testgetarray = function () {
         this.array_coordinates['oo']="jjknk";
-        return this.array_coordinates;
+        return SharedPaintBoard.array_coordinates;
     }
 
 
